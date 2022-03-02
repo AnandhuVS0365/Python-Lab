549848fe7347644a5857597ea7e88f552b03569e
AnandhuVS0365 <anandhuvskesari@gmail.com>
2022-03-02 23:12:38 +0530

    modified

--- CO1_2.py
+++ CO1_2.py
@@ -7,4 +7,4 @@ final_year=int(input("enter final year"))
 print("Leap years range from {} to {}".format(year1,final_year))
 for year in range(year1,final_year+1):
     if ((year%4==0) and (year%400==0 or year%100!=0)):
-        print(year)
+        print(year) 
